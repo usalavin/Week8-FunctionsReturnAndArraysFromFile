27a8d89b6d7d05f02b5d6c7b9575f09a13d2d291
unknown <Uljana alaviba>
2021-11-21 19:23:53 +0200

    add new projects

---
+++ ChuckNorris/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace ChuckNorris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomPhrase;
+            randomPhrase = GetRandomFromFile("chuck (1).txt");
+           
+            Console.WriteLine(randomPhrase); 
+        }
+
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+
+            return randomIndex;
+        }
+
+        private static string GetRandomFromFile(string fileName)
+        {
+            string filePath = $@"C:\Users\...\samples\chuck (1).txt";
+
+            string [] dataFromFile = File.ReadAllLines(filePath);
+            string randomElement = dataFromFile[GenerateRandomIndex(dataFromFile)];
+
+            return randomElement;
+
+        }
+
+    }
+
+
+}
